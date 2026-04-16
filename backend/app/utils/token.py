eb14f63e05fe920d38d658a1a0978a757d5a516b
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2026-04-16 17:33:48 +0300

    feat(backend): implement JWT authentication and protect member routes

---
+++ backend/app/utils/token.py
@@ -0,0 +1,25 @@
+import jwt
+import datetime
+import os
+
+SECRET_KEY = os.getenv("SECRET_KEY", "super-secret")
+
+def generate_access_token(user):
+    payload = {
+        "user_id": user.id,
+        "email": user.email,
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=15)
+    }
+
+    token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+    return token
+
+
+def decode_token(token):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return payload
+    except jwt.ExpiredSignatureError:
+        raise ValueError("Token expired")
+    except jwt.InvalidTokenError:
+        raise ValueError("Invalid token")
