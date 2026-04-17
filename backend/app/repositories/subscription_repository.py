1cf518beb801acae43b63f9f5cfac1d04db01e79
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2026-04-17 14:05:51 +0300

    feat: add subscription repository layer for managing MembershipSubscription data

---
+++ backend/app/repositories/subscription_repository.py
@@ -0,0 +1,22 @@
+from app.models import MembershipSubscription
+from app.extensions import db
+
+
+class SubscriptionRepository:
+    
+    def create(self, subscription):
+        db.session.add(subscription)
+        db.session.commit()
+        return subscription
+
+    def get_by_id(self, subscription_id):
+        return MembershipSubscription.query.get(subscription_id)
+
+    def get_all(self):
+        return MembershipSubscription.query.all()
+
+    def get_by_member(self, member_id):
+        return MembershipSubscription.query.filter_by(member_id=member_id).all()
+
+    def update(self):
+        db.session.commit()
