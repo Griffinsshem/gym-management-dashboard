946f64bfcb400c62948dc6fe07fcb97dc9b91bd9
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2026-04-19 06:53:56 +0300

    feat: update member repository

--- backend/app/repositories/member_repository.py
+++ backend/app/repositories/member_repository.py
@@ -6,28 +6,14 @@ class MemberRepository(BaseRepository):
     model = Member
 
     def get_all(self):
-        return Member.query.filter_by(is_active=True).all()
+        return self.model.query.filter_by(is_active=True).all()
 
     def get_by_id(self, member_id):
-        return Member.query.filter_by(id=member_id, is_active=True).first()
+        return self.model.query.filter_by(id=member_id, is_active=True).first()
 
     def get_by_phone(self, phone):
-        return Member.query.filter_by(phone=phone).first()
-
-    def create(self, data):
-        member = Member(**data)
-        self.db.session.add(member)
-        self.db.session.commit()
-        return member
-
-    def update(self, member, data):
-        for key, value in data.items():
-            setattr(member, key, value)
-
-        self.db.session.commit()
-        return member
+        return self.model.query.filter_by(phone=phone).first()
 
     def deactivate(self, member):
         member.is_active = False
-        self.db.session.commit()
-        return member
+        return self.update(member, {})
