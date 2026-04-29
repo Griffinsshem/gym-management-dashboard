faabbfbb40600ad3a807d8acdeb6b1f8b27b64b7
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2026-04-29 09:32:55 +0300

    feat(run): add database URL logging in app context

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,15 @@
 from app import create_app
 from flask_cors import CORS
+from app.extensions import db
 
 app = create_app()
 
 CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+with app.app_context():
+    print("====================================")
+    print("DB URL:", db.engine.url)
+    print("====================================")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
